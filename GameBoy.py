f5af510be99fd7e74c03e5df3fd3b9d2fdb24a13
Kevin Edington <kevin.edington@snapiot.com>
2018-12-17 15:54:49 -0800

    Update to properly loop through all pages and handle logic

--- GameBoy.py
+++ GameBoy.py
@@ -5,10 +5,16 @@ from oauth2client import file, client, tools
 from time import sleep
 from pprint import pprint 
 from base64 import b64decode
-from collections import defaultdict
+import re
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
+
+# Parses out all non Alphabetic or spaces and returns a list
+# of all words in the string 
+def parse_string(dirty_string):
+    parsed_words = re.sub(r'\s*[^A-Za-z]+\s*', ' ', dirty_string).lower().split()
+    return parsed_words.split() 
 
 def main():
     """Shows basic usage of the Gmail API.
@@ -29,35 +35,47 @@ def main():
     counter = 0
     msg_counter = 0
     headers = ['Subject']
-    words = defaultdict(int)
 
-	# Loop through while the nextPageToken is not in the msg_list
-    while 'nextPageToken' in msg_list:
+    words = set()
+    gameboy_words = set()
+
+    # Loop through while the nextPageToken is not in the msg_list
+    while True:
         counter += 1
         message_ids = msg_list['messages']
         for message_node in message_ids:
             message_id = message_node['id'] 
             message = service.users().messages().get(userId='me', id=message_id, fields='payload(headers, parts/body/data)').execute()
 
-            # Extract Subject and Body 
+            # Extract Subject from the headers
             subject = [header['value'] for header in message['payload']['headers'] if header['name'] == 'Subject'][0]
-            print(subject)
+            subject_words = re.sub(r'\s*[^A-Za-z]+\s*', ' ', subject).lower().split()
+            
+            # Extract the Budy and Decode it into plane text
             if 'parts' in message['payload']:
                 body_raw = message['payload']['parts'][0]['body']['data'].replace("-", "+").replace("_", "/")
                 body = b64decode(body_raw).decode('utf-8')
-                unique_words = ''.join(filter(lambda x: x.isalpha() or x.isspace(), body)).lower().split()
-                for word in unique_words:
-                    if len(word) < 15:
-                        words[word] += 1
-        pprint(words)
-        print(len(words))
-        break
-        page_token = msg_list['nextPageToken']
-        
-        sleep(1) # Sleep For a second as to no exceed quota units 
-        msg_list = service.users().messages().list(userId='me', pageToken=page_token).execute()
-        
+                body_words = re.sub(r'\s*[^A-Za-z]+\s*', ' ', body).lower().split()
+                total_words = subject_words + body_words
+            else:   
+                total_words = body_words
 
+            # Loop through all the words adding them to a set that keeps track of unique words and a set that holds repeated words
+            for word in total_words:
+                if word not in words:
+                    if word not in gameboy_words:
+                        gameboy_words.add(word)
+                    else:
+                        gameboy_words.remove(word)
+                        words.add(word)
+
+        # If there is still another page continue to get the next list of emails
+        if 'nextPageToken' in msg_list:
+            page_token = msg_list['nextPageToken']
+            msg_list = service.users().messages().list(userId='me', pageToken=page_token).execute()
+        else: # all emails have been read 
+            print(gameboy_words)
+            break
 
 if __name__ == '__main__':
     main()
